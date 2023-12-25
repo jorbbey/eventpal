08cf387ba95a8d4754877624022afcd9ad33f965
Jorbbey <jorbbey.gmail.com>
2023-12-25 22:22:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./input.css";
+
+const App = () => {
+  return <>
+  <h1 className="text-3xl text-purple-800 bg-blue-400 h-60">This is the tailwind test for Eventpal app (that name will be changed sef)</h1>
+  </>;
+};
+
+export default App;
